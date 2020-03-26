e0b9d08031a62b5808505818dd8f1b0efc5a796a
carlsachs <sachscarl@gmail.com>
2020-03-26 14:50:38 -0400

    changed enhancer spec

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,9 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const {repair, succeed, fail, get} = require('./enhancer.js');
+
+describe('enhancer', function (){
+    describe('repair()', function(){
+        it('Should return a new item with the durability restored to 100', function() {
+
+        })
+    })
+}) 
